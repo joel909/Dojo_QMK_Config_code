ade5b913a879104d7663c2d32f54e445ea77df5a
Joel Joby <joeljobyp@gmail.com>
2024-07-04 17:58:47 +0530

    updated added 2 more cols now cols 1,2,3,4,5, and 1,2,3,4,5, rows work

--- dojo/keymaps/default/keymap.c
+++ dojo/keymaps/default/keymap.c
@@ -21,10 +21,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┴───┘
      */ 
     [0] = LAYOUT_ortho_4x4(
-        KC_ESC,  KC_1,KC_2,  
-        KC_TAB,   KC_Q,KC_W,
-        KC_CAPS_LOCK, KC_A,KC_S,
-        KC_LEFT_SHIFT, KC_Z,KC_X,
+        KC_ESC,  KC_1,KC_2,KC_3,KC_4,  
+        KC_TAB,   KC_Q,KC_W,KC_E,KC_R,
+        KC_CAPS_LOCK, KC_A,KC_S,KC_D,KC_F,
+        KC_LEFT_SHIFT, KC_Z,KC_X,KC_C,KC_V,
         KC_LEFT_CTRL, KC_LEFT_GUI,KC_LEFT_ALT
 
 
